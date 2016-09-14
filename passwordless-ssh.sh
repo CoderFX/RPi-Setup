8b4cadb1381477a2476eca175f3078ff2c60fda6
Mindaugas <gelumind@gmail.com>
2016-09-14 17:34:14 +0300

    Update passwordless-ssh.sh

--- passwordless-ssh.sh
+++ passwordless-ssh.sh
@@ -4,6 +4,9 @@ sudo nano /etc/ssh/sshd_config
 #Uncomment and change PasswordAuthentication to no
 <command to find correct line and change it> <TBD>
 
+#Change "UsePAM yes" to "UsePAM no" - This will disable password authentication (PAM)
+<command to find correct line and change it> <TBD>
+
 #Restart sshd
 sudo service restart sshd
 
