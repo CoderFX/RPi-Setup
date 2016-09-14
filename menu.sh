366369ada50d15b56617e1ad47b0b379548ea6e7
Mindaugas <gelumind@gmail.com>
2016-09-14 16:48:51 +0300

    Update menu.sh

--- menu.sh
+++ menu.sh
@@ -15,7 +15,24 @@ do
   echo -------------------------------
   echo This is the script named \"menu\"
   echo -------------------------------
-  echo -e 'S: Show Network config '
+  echo -e '1: Change default password for pi'
+  echo -e '2: Run raspi-config and setup hostname, overclocking, other settings'
+  echo -e '3: Add RSA 4096 bit SSH keys'
+  echo -e '4: Create passwordless SSH connection'
+  echo -e '5: Create Wifi access point (requires 2 wifi adapters)'
+  echo -e '6: Create Ad-hoc wifi network'
+  echo -e '7: Create Ad-hoc wifi network'
+  echo -e '8: Create Ad-hoc bluetooth network (for SSH connection)'
+  echo -e '9: Add option to SSH via USB'
+  echo -e '10: Add VPN settings, so RPi always connects to home LAN network if possible'
+  echo -e '11: Auto mount network NAS at startup'
+  echo -e '12: ===> Option to run read and run scripts from NAS?'
+  echo -e '13: Setup backups for RPi to NAS'
+  echo -e '14: Setup Alexa (might be complicated)'
+  echo -e '15: Install important applications'
+  echo -e '16: >Python (for GPIO)''
+  echo -e '17: >Nginx'
+    echo -e 'S: Show Network config '
   echo -e 'I: IP Address '
   echo -e 'C: Change Network config'
   echo -e 'N: Change Network Name'
@@ -37,6 +54,27 @@ do
   elif nocase "$opt" "N"
   then
     sudo ./change.network.name
+  
+  #1: Change default password for pi
+  elif nocase "$opt" "1"
+  then
+    sudo passwd pi
+    
+  #2: Run raspi-config and setup hostname, overclocking, other settings
+  elif nocase "$opt" "2"
+  then
+    sudo raspi-config
+    
+  #3: Add RSA 4096 bit SSH keys <TBD>
+  elif nocase "$opt" "3"
+  then
+    sudo ./ssh-keys.sh
+  
+  #4: Create passwordless SSH connection <TBD>
+  elif nocase "$opt" "3"
+  then
+    sudo ./passwordless-ssh.sh
+  
   elif nocase "$opt" "Q"
   then
     exit=TRUE
@@ -53,8 +91,8 @@ Create passwordless SSH connection
 Create Wifi access point (requires 2 wifi adapters)
 Create Ad-hoc wifi network
 Create Ad-hoc bluetooth network (for SSH connection)
-Add option to
-Add VPN settings, so RPi is always in home LAN network
+Add option to SSH via USB
+Add VPN settings, so RPi always connects to home LAN network if possible
 Auto mount network NAS at startup
 ===> Option to run read and run scripts from NAS?
 Setup backups for RPi to NAS
