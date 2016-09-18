6ce9582a58c371ebff6fbecf85cfd5bd9c071ce4
CoderFX <gelumind@gmail.com>
2016-09-18 22:46:29 +0300

    start

--- copy-ssh-keys.sh
+++ copy-ssh-keys.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 echo -e 'Make sure you have plugged in USB and have your keys stored there'
-echo -e 'Note that this will remove any previously stored keys'
+echo -e 'Note that this not remove any previously stored keys, so you can feel safe :)'
 
 cd ~
 #Make sure ssh keys are installed
 sudo install -d -m 700 ~/.ssh
 sudo mkdir -p ~/.ssh
-sudo cp /mnt/usb/authorized_keys ~/.ssh/authorized_keys
+sudo cat /mnt/usb/id_rsa.pub >> ~/.ssh/authorized_keys
 sudo chmod 600 ~/.ssh/authorized_keys
