76eeb66e4037df66730cfa8d2a8278fe4df8dfce
CoderFX <gelumind@gmail.com>
2016-09-18 23:20:07 +0300

    fix

--- copy-ssh-keys.sh
+++ copy-ssh-keys.sh
@@ -4,8 +4,7 @@ echo -e 'Make sure you have plugged in USB and have your keys stored there'
 echo -e 'Note that this not remove any previously stored keys, so you can feel safe :)'
 
 cd ~
-#Make sure ssh keys are installed
 sudo install -d -m 700 ~/.ssh
 sudo mkdir -p ~/.ssh
-sudo cat /mnt/usb/id_rsa.pub >> ~/.ssh/authorized_keys
+sudo cat /mnt/usb/id_rsa.pub >> /home/pi/.ssh/authorized_keys
 sudo chmod 600 ~/.ssh/authorized_keys
